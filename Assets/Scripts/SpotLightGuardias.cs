479f67627fd2aef75e56cdd119652ae6e4a879cc
DylanTsai1 <46960341@est.ort.edu.ar>
2022-08-03 14:54:01 -0300

    Inicio de Raycast para identificar al jugador

---
+++ Assets/Scripts/SpotLightGuardias.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotLightGuardias : MonoBehaviour
+{
+    public GameObject jugador;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, jugador.transform.position);
+        Debug.DrawRay(transform.position, jugador.transform.position);
+
+    }
+}
