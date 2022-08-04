f1462b70adb714afa8a000496803aeeef2da59b9
jereabu <jeremiasabulafia@gmail.com>
2022-08-04 18:11:40 -0300

    Added Crouch

--- Assets/Scripts/Agacharse.cs
+++ Assets/Scripts/Agacharse.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Agacharse : MonoBehaviour
 {
-    public GameObject jugador;
-    Vector3 posJugador;
+    float Altura = 1;
+    float AlturaAgachado = .5f;
+    float Altura2 = 1;
+    float AlturaAgachado2 = .5f;
+    BoxCollider colision;
+    CharacterController colision3;
     // Start is called before the first frame update
     void Start()
     {
+        colision = GetComponent<BoxCollider>();
+        colision3 = GetComponent<CharacterController>();
+        Altura = colision.size.y;
+        Altura2 = colision3.height;
         
     }
 
@@ -16,8 +24,20 @@ public class Agacharse : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(KeyCode.LeftControl))
-        {
-            transform.position = new Vector3(x, y - 3, z);
-        }
+            Agachate();
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+            Levantate();
+
+        
+    }
+    void Agachate()
+    {
+        colision.size = new Vector3 (Altura, AlturaAgachado, Altura);
+        colision3.height = AlturaAgachado2;
+    }
+    void Levantate()
+    {
+        colision.size = new Vector3 (Altura, Altura, Altura);
+        colision3.height = Altura2;
     }
 }
