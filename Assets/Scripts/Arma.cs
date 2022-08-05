4f6a3991545d80a04b452768ec9f851a9bf3ae38
Jereabu <jeremiasabulafia@gmail.com>
2022-08-05 08:59:26 -0300

    Arma añadida con su función

---
+++ Assets/Scripts/Arma.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arma : MonoBehaviour
+{
+    public GameObject bala;
+    public GameObject armajugador;
+    float speed = 2;
+    float lifetime = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(bala, lifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Instantiate(bala, armajugador.transform.position, armajugador.transform.rotation);
+        }
+        bala.transform.position += bala.transform.forward * speed;
+    }
+    void OnCollisionEnter()
+    {
+         Destroy(bala);
+    }
+}
