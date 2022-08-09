73738c0dc818e671d18d31b38a50792af2012283
Jereabu <jeremiasabulafia@gmail.com>
2022-08-09 14:22:38 -0300

    Fixed sliding rotation

--- Assets/Scripts/Agacharse.cs
+++ Assets/Scripts/Agacharse.cs
@@ -15,8 +15,9 @@ public class Agacharse : MonoBehaviour
     private bool SlideDebounce;
     private int TimeStamp;
     Rigidbody m_rigidbody;
-    public float time;
+    public float time = 3;
     public float ctime;
+    public float rotationSpeed = 0;
     // Start is called before the first frame update
 
 
@@ -32,44 +33,51 @@ public class Agacharse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ctime -= Time.deltaTime;
+
         if (Input.GetKey(KeyCode.LeftControl))
             Agachate();
-        else if (Input.GetKeyDown(KeyCode.Z))
+        else if (Input.GetKey(KeyCode.Z))
         {
             Deslizate();
+            ctime -= Time.deltaTime;
         }
-        else if (Input.GetKeyUp(KeyCode.LeftControl))
+        else if (Input.GetKeyUp(KeyCode.LeftControl) || ctime < 0)
+        {
             Levantate();
-        
-
+        }
     }
    private void Agachate()
     {
         colision.size = new Vector3 (Altura, AlturaAgachado, Altura);
         colision3.height = AlturaAgachado2;
-        jugador.m_RunSpeed = 10f;
+        jugador.m_WalkSpeed = 5f;
         m_rigidbody.constraints = RigidbodyConstraints.None;
+        jugador.m_MouseLook.XSensitivity = 2;
+        jugador.m_MouseLook.YSensitivity = 2;
     }
     private void Levantate()
     {
         colision.size = new Vector3 (Altura, Altura, Altura);
         colision3.height = Altura2;
-        jugador.m_RunSpeed = 10f;
+        jugador.m_WalkSpeed = 5f;
         m_rigidbody.constraints = RigidbodyConstraints.None;
+        jugador.m_MouseLook.XSensitivity = 2;
+        jugador.m_MouseLook.YSensitivity = 2;
     }
 
     private void Deslizate()
     {
         
-        if(ctime > 0)
+        if(ctime >= 0)
         {
         colision.size = new Vector3(Altura, AlturaAgachado, Altura);
         colision3.height = AlturaAgachado2;
-        jugador.m_RunSpeed = 15f;
-        m_rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        jugador.m_WalkSpeed = 15f;
+        jugador.m_MouseLook.XSensitivity = 0;
+        jugador.m_MouseLook.YSensitivity = 0;
+            m_rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
         }
-        else if(ctime < 0)
+        else if(ctime <= 0)
         {
             ctime = time;
             Levantate();
