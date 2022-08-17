5ecc9a9f2c34555896152037011c8605ea27e030
DylanTsai1 <46960341@est.ort.edu.ar>
2022-08-17 14:37:29 -0300

    Almost fixed EntrarCaja

--- Assets/Scripts/EntrarCaja.cs
+++ Assets/Scripts/EntrarCaja.cs
@@ -9,31 +9,43 @@ public class EntrarCaja : MonoBehaviour
     public FirstPersonController jugadorcontroller;
     CharacterController controller;
     public GameObject caja;
+    [SerializeField] Vector3 posicionjugadorcaja;
+    
 
     // Start is called before the first frame update
     void Start()
     {
          controller = GetComponent<CharacterController>();
+         controller.stepOffset = 100;
 
     }
 
 // Update is called once per frame
 private void Update()
     {
-        
-    }
-    void OnTriggerStay()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (jugador.transform.position != caja.transform.position)
         {
-            jugador.transform.position = caja.transform.position;
-            jugadorcontroller.CollisionFlags = 0;
-            controller.enabled = !controller.enabled;
-        }
-        else if (Input.GetKeyDown(KeyCode.Z))
-        {
-            jugador.transform.position = new Vector3(1, 1, 5);
             controller.enabled = controller.enabled;
         }
+        // 1. mover el reigidbody 2. desactivar solo lascolisiones del charactercpntroller 3. ontrigger exit vuelvaelcharacter controller
+    }   
+
+    void OnTriggerStay()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            posicionjugadorcaja = new Vector3(jugador.transform.position.x, jugador.transform.position.y, jugador.transform.position.z);
+            jugador.transform.position = caja.transform.position;
+            jugadorcontroller.CollisionFlags = 0;
+            controller.stepOffset = 100;
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            jugador.transform.position = posicionjugadorcaja;
+            //controller.enabled = controller.enabled;
+            controller.stepOffset = 0.3f;
+        }
+        
     }
 }
