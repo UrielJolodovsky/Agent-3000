b843e810a5491a1d99996e839e60a770ed4a2b0a
Uriel Jolodosvky <46960341@est.ort.edu.ar>
2022-10-18 01:38:17 -0300

    Open a box with animation finished and enter the lifting with animation almost finished

---
+++ Assets/Scripts/AbrirCaja.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class AbrirCaja : MonoBehaviour
+{
+    public Text cajaAbrir;
+    [SerializeField] bool posibilidad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posibilidad = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (posibilidad && Input.GetKeyDown(KeyCode.E))
+        {
+
+            this.gameObject.tag = "CajaAbierta";
+            this.GetComponent<Animation>().Play("Caja");
+            posibilidad = false;
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (this.gameObject.tag == "Caja")
+            {
+                cajaAbrir.enabled = true;
+                posibilidad = true;
+            }
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            cajaAbrir.enabled = false;
+            posibilidad = false;
+        }
+    }
+}
---
+++ Assets/Scripts/EntrarAscensorAnim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntrarAscensorAnim : MonoBehaviour
+{
+    [SerializeField] bool posibilidad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posibilidad = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (posibilidad && Input.GetKeyDown(KeyCode.E))
+        {
+            
+            posibilidad = false;
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            posibilidad = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            posibilidad = false;
+        }
+    }
+}
