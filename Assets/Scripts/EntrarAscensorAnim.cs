9b49667bb8919cd18c20211d760f56f0508cf8be
UrielJolodovsky <46960341@est.ort.edu.ar>
2022-10-19 11:28:42 -0300

    EntrarAscensor almost finished

--- Assets/Scripts/EntrarAscensorAnim.cs
+++ Assets/Scripts/EntrarAscensorAnim.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EntrarAscensorAnim : MonoBehaviour
 {
+    public Text entrarAscensor;
     [SerializeField] bool posibilidad;
     // Start is called before the first frame update
     void Start()
@@ -16,15 +18,21 @@ public class EntrarAscensorAnim : MonoBehaviour
     {
         if (posibilidad && Input.GetKeyDown(KeyCode.E))
         {
-            
+            this.GetComponent<Animation>().Play("Ascensor");
             posibilidad = false;
+            entrarAscensor.enabled = false;
+            this.gameObject.tag = "AscensorAbierto"; 
         }
     }
     void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            posibilidad = true;
+            if (this.gameObject.tag == "Ascensor")
+            {
+                entrarAscensor.enabled = true;
+                posibilidad = true;
+            } 
         }
     }
     void OnTriggerExit(Collider other)
