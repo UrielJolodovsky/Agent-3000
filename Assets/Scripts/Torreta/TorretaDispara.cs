c6c0f3dd8a4eff996d1a7aa668baac81f1d5af5b
jereabu <jeremiasabulafia@gmail.com>
2022-10-29 18:45:21 -0300

    added main menu details and started working on torreta

--- Assets/Scripts/Torreta/TorretaDispara.cs
+++ Assets/Scripts/Torreta/TorretaDispara.cs
@@ -6,7 +6,6 @@ public class TorretaDispara : MonoBehaviour
 {
     [SerializeField] GameObject player;
     [SerializeField] Vector3 posJugador;
-    [SerializeField] AbrirPuertaLevel3 abrirPuerta;
     [SerializeField] public bool puertaAbierta;
     [SerializeField] public bool activadas;
     [SerializeField] GameObject Bala1;
@@ -15,26 +14,28 @@ public class TorretaDispara : MonoBehaviour
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        puertaAbierta = false;
-        abrirPuerta = GetComponent<AbrirPuertaLevel3>();
-        Bala1 = this.transform.Find("SalidaBala1").gameObject;
-        Bala2 = this.transform.Find("SalidaBala2").gameObject;
+        puertaAbierta = AbrirPuertaLevel3.puertaAbierta;
+        //Bala1 = gameObject.transform.Find("SalidaBala1").gameObject;
+        //Bala2 = gameObject.transform.Find("SalidaBala2").gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
         posJugador = player.transform.position;
-        puertaAbierta = abrirPuerta.puertaAbierta;
+        puertaAbierta = AbrirPuertaLevel3.puertaAbierta;
         if (puertaAbierta)
         {
             activadas = true;
         }
         if (activadas)
         {
+            Bala1.SetActive(true);
+            Bala2.SetActive(true);
+            //gameObject.transform.LookAt(player.transform);
             // Funcionamiento de las torretas
-            this.transform.LookAt(posJugador);
-
+            //var b = Instantiate(Bala1, Bala1.transform.position, Bala1.transform.rotation);
+            //var c = Instantiate(Bala2, Bala2.transform.position, Bala2.transform.rotation);
         }
     }
 }
