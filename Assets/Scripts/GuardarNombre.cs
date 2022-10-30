5afb54ac6672fcfa08b8ebd11c8783219f93355c
jereabu <jeremiasabulafia@gmail.com>
2022-10-30 16:06:17 -0300

    Added main menu and Intro textures

---
+++ Assets/Scripts/GuardarNombre.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuardarNombre : MonoBehaviour
+{
+    public static string nombreJugador;
+    public InputField nombreInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(nombreInput + nombreJugador);
+    }
+    public void GuardarNombreJugador()
+    {
+        nombreJugador = nombreInput.text;
+    }
+}
