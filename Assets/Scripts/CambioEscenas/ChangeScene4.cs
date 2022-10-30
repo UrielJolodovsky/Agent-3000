8538a5b8e4758638c06caa6318ed5007ede9d873
jereabu <jeremiasabulafia@gmail.com>
2022-10-30 15:06:03 -0300

    Added computer functions

---
+++ Assets/Scripts/CambioEscenas/ChangeScene4.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene4 : MonoBehaviour
+{
+    public void LoadScene(string Nivel3)
+    {
+        SceneManager.LoadScene(Nivel3);
+    }
+}
